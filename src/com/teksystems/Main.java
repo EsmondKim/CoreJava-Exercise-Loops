25ba613df638662163d8593b3174c364fca2dfaf
EsmondKim <ekelectronicmail@gmail.com>
2022-03-01 20:37:42 -0600

    Completed all except palindrome and fibonacci.

--- src/com/teksystems/Main.java
+++ src/com/teksystems/Main.java
@@ -1,12 +1,17 @@
 package com.teksystems;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
 //        loopOneToTen();
 //        whileLoopTo100();
 //        doWhileLoop();
 //        loopMultiplesOfFive();
-        loopFivesLessThan50();
+//        loopFivesLessThan50();
+//        nestedLoop();
+//        palindrome();
+        nestedForLoopVar();
     } //main
 
     public static void loopOneToTen() {
@@ -48,6 +53,36 @@ public class Main {
         System.out.println("You're done son!");
     }//loopFivesLessThan50()
 
-    public static void
+    public static void nestedLoop() {
+        for (int week = 1; week < 3; ++week) {
+            System.out.printf("Week: %d\n", week);
+            for(int day = 1; day < 6; ++day) {
+                System.out.printf("Day: %d\n", day);
+            } //nested loop
+            System.out.println();
+        }//outer loop
+    }//nestedLoop()
+
+    public static void palindrome() {
+        for (int i = 10; i < 200; i++) {
+            StringBuilder sb = new StringBuilder();
+            sb.append(i);
+            StringBuilder rev = new StringBuilder();
+            rev = sb.reverse();
+            System.out.println(rev);
+        }//for loop
+    }//palindrome()
+
+    public static void nestedForLoopVar() {
+        for (int i = 1; i < 6; i++) {
+            System.out.printf("Here's i %d\n", i);
+            for(int j =1; j < 11; j++) {
+                System.out.printf("Here's j: %d\n", j);
+            }//inner for loop
+            System.out.printf("Loop's run this many times: %d\n", i);
+        }//outer for loop
+    }//nestedForLoopVar()
+
+
 
 } //Main
